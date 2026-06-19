d5286f1da157cd4de39c4ec512b6af1c44a75da5
vrma <vrmachado@gmail.com>
2026-06-19 18:55:18 +0200

    para Yodalis

--- framework-de-colecciones/src/main/java/com/example/App.java
+++ framework-de-colecciones/src/main/java/com/example/App.java
@@ -4,7 +4,39 @@ public class App {
 	
     public static void main(String[] args) {
     	
+    	/* Existen 3 formas de iterar o recorrer una coleccion, siendo una de ellas la unica que 
+    	 * permite eliminar un elemento de la coleccion mientras se recorre 
+    	 * 
+    	 * Traversing Collections:
+    	 * https://docs.oracle.com/javase/tutorial/collections/interfaces/collection.html
+    	 * 
+    	 * Formas de recorrer una coleccion
+    	 * 
+    	 * 1- Mediante un Iterador (Iterator en Ingles)
+    	 * 2- Mediante una sentencia for mejorada
+    	 * 3- Mediante Operaciones de Agregado (Metodos de la clase Stream, Expresiones Lambda,
+    	 * 	  Metodos por referencia, en fin, PROGRAMACION FUNCIONAL)
+    	 * 
+    	 * Y la ultima, que no por se la ultima es la menos importante, sino todo lo contrario, es la
+    	 * sugerida, y por ello la dejamos para el final
+    	 * */
       	
+    	// Uso de la interfaz Iterator para eliminar un elemento de la coleccion mientras se recorre.
+    	// Para ello, como Ejercicio # 1 del viernes 19 de Junio, se pide:
+    	/* Crear una lista de Persona, mutable, donde cada Persona sea un record, con las siguientes propieades
+    	 * 
+    	 * - nombre
+    	 * - apellido1
+    	 * - apellido2
+    	 * - genero
+    	 * - fechaNacimiento
+    	 * - salario
+    	 * 
+    	 * La lista de Persona tiene que contener 5 personas
+    	 * 
+    	 * -- Para hacer con el Profesor
+    	 * Una vez creada la lista, la recorremos y eliminamos a todas las personas del genero HOMBRE, 
+    	 * que tengan salario inferior a la media */
     }
 }
 
