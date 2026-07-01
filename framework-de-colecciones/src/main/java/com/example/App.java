1a821e260755d434e2f6ccd70d03c0dc7add6a2a
vrma <vrmachado@gmail.com>
2026-07-01 21:02:44 +0200

    Depurando el codigo

--- framework-de-colecciones/src/main/java/com/example/App.java
+++ framework-de-colecciones/src/main/java/com/example/App.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.Set;
@@ -19,42 +21,6 @@ import java.util.stream.Stream;
 public class App {
 
 	public static void main(String[] args) {
-
-		/*
-		 * Operaciones de Agregado, para recorrer y operar con los elementos de una
-		 * coleccion
-		 * 
-		 * En el siguiente enlace se hace una breve introduccion a las Operaciones de
-		 * Agregado
-		 * https://docs.oracle.com/javase/tutorial/collections/interfaces/collection.
-		 * html
-		 * 
-		 * Y en el link siguiente ya se abordan con mas profundidad:
-		 * https://docs.oracle.com/javase/tutorial/collections/streams/index.html
-		 * 
-		 * Las Operaciones de Agregado implican convertir la coleccion en un flujo
-		 * (Stream) de elementos que comienzan a circular por una tuberia (pipeline),
-		 * que podemos entenderla como una linea de produccion, por ejemplo, una cadena
-		 * donde se rellenan latas de conserva, que en cada punto de la cadena se hace
-		 * una operacion diferente, sobre la lata de conserva, es decir, un operador la
-		 * rellena, otro le pone la etiqueta otro la cierra, otro la mete en una caja,
-		 * etc.
-		 * 
-		 * Concretamente, la tuberia es una secuencia de operaciones de agregado, es
-		 * decir, operaciones que agrupan los elementos que van circulando por la
-		 * tuberia para hacer algun tipo de operacion.
-		 * 
-		 * Especificamente, la tuberia son los metodos de la clase Stream, que tiene un
-		 * origen que puede ser una coleccion, un array, un socket de red, etc., tambien
-		 * tiene cero, una o varias operaciones intermedias y solamente una operacion
-		 * terminal, al final de la tuberia, que reducirá todos los elementos que van
-		 * pasando por la tuberia a un solo elemento, o a una nueva coleccion
-		 * 
-		 * Las Operaciones de Agregado no es una sintaxis bonita, sino el soporte de la
-		 * JVM que tienen detras estas operaciones es muy superior a todo lo que
-		 * anteriormente existia con los bucles explicitos (entiendase for clasico y
-		 * mejorado, while, do while, etc)
-		 */
 
 		Persona persona1 = Persona.builder().nombre("Maria").apellido1("lopez").apellido2("fernandes")
 				.genero(Genero.MUJER).fechaNacimiento(LocalDate.of(1985, Month.OCTOBER, 20))
@@ -83,77 +49,76 @@ public class App {
 		personas.add(persona4);
 		personas.add(persona5);
 
-		/* Se puede ordenar una coleccion a medida que se recorre, y no dejarla permanentemente
-		 * ordenada como sucede con el metodo sort() de la clase Collections */
 		
-		// Ejemplo # 1. Ordenar la coleccion de personas mientras se recorre, segun el 
-		// el orden natural
-		
-		System.out.println("Ejemplo # 1");
-		personas.stream()
-			.sorted()
-			.forEach(System.out::println);
-		
-		// Ejemplo # 2. Ordenar la coleccion de personas mientras se recorre por el salario
-		// de la persona
-		System.out.println("Ejemplo # 2");
-		personas.stream()
-			.sorted(Comparator.comparing(Persona::salario))
-			.forEach(System.out::println);
-
-		
-		/* Ejercicio # 1 del martes 30 de Junio.
+		/**
+		 * ¿Que es una coleccion Map, que tambien se le llama un mapa?
+		 * Es una interface que relaciona claves con Valor en cada entrada
+		 * del mapa, y las claves no pueden repetirse
 		 * 
-		 * Ordenar el listado de personas, por salario de mayor a menor salario */
+		 * Se dice que un mapa (Map Interface) NO es una  verdadera coleccion
+		 * porque no hereda de la interfaz Collection, pero puede ser tratada como
+		 * una coleccion si se utilizan las vistas de colecciones (Collections Views)
+		 */
 		
-		// Suministrado por Jakelin
+		/* Ejemplo de creacion de un Map, es decir, una coleccion que almacena parejas de
+		 * clave y valor, para ello vamos a contar la frecuencia de ocurrencia de 
+		 * nombres que se reciben como argumentos de la aplicacion cuando se ejecuta */
 		
-		 
-		/* Ordenar el listado de personas, por salario de mayor a menor salario */
-         System.out.println("ejercicio 1 ");
-         
-         personas.stream()
-	        .sorted(Comparator.comparing(Persona::salario).reversed())
-	        .forEach(System.out::println);
+		// Primero: Convertir el array de argumentos que recibe la aplicacion
+		// cuando se ejecuta, es decir, la variable args de tipo String[], que recibe 
+		// como parametro el metodo main(), en una coleccion List
 		
-		/* Ejercicio # 2. Ordenar por Genero, primero y luego por salario, a medida que se
-		 * recorre la coleccion de persona para mostrarla */
-         
-         // Suministrado por Carlos
-        System.out.println("Ejercicio #2");
- 		personas.stream()
- 			.sorted(Comparator.comparing(Persona::genero)
- 					.thenComparing(Persona::salario))
- 			.forEach(System.out::println);
+		List<String> nombres = Arrays.asList(args);
+		
+		// Comprobar si nombres posee todos los argumentos que recibe la aplicacion
+		// cuando se ejecuta
+		
+		nombres.forEach(System.out::println);
 		
 		
-		/* Ejercicio # 3. Mostrar la persona que tiene el mayor salario de todas las 
-		 * personas. */
- 		System.out.println("Ejercicio 3. Persona con el mayor salario");
-		Optional<Persona> personaOptional = personas.stream()
-			.max(Comparator.comparing(Persona::salario));
+		// Segundo: Declarar el mapa, especificando el tipo de datos de las claves y de
+		// los valores del mapa
 		
-		if (personaOptional.isPresent())
-		    System.out.println(personaOptional.get());
+		Map<String, Integer> m = new HashMap<String, Integer>();
 		
-		/* Ejercicio # 4. 
+		/**
+		 * Una de las clases que implementan la interface Map es HashMap, que no permite
+		 * ordenar la claves del mapa, pero ofrece un alto rendimiento.
 		 * 
-		 * Recuperar la persona del genero MUJER, que tenga el menor salario */
-		Optional<Persona>  optionalDePersona =  personas.stream()
-			.filter(persona -> persona.genero().equals(Genero.MUJER))
-			.min(Comparator.comparing(Persona::salario));
+		 * Si queremos ordenar las claves del mapa hay que utilizar un TreeMap que si
+		 * permite ordenamiento
+		 */
 		
-		if (optionalDePersona.isPresent()) {
+		// ¿Que diferencia hay entre String (cadena de caracteres) y Stream (flujo) ? 
+		// String es una clase que sirve como tipo de dato, cualquier variable de tipo
+		// String su valor tiene que estar entre comilla dobles
+		// String nombre = "Elida 22323@@ #";
+		
+		Stream<String> flujoDeString = nombres.stream();
+		
+		/***
+		 * A continuacion vamos a crear la coleccion mapa recorriendo la lista de nombres
+		 * con una sentencia for mejorada y comprobaremos, posteriormente, que es
+		 * mucho menos codigo si hacemos lo mismo con operaciones de agregado para 
+		 * recorrer la coleccion de nombres
+		 * 
+		 */
+		
+		for (String nombre : nombres) {
 			
-			// Quiere decir que en la "cajita" del Optional esta presente  la persona
-			// que es del genero MUJER y que tiene el menor salario
+			// Comprobar si el nombre ya esta en el mapa m
+			// . Lo cual hacemos utilizando el metodo get
+			// que busca, en el mapa, el valor asociado a la clave
+			// que se le pasa como parametro, si encuentra la clave
+			// devuelve el valor asociado a la misma y de lo contrario
+			// devuelve null
+			Integer frecuenciaDeOcurrencia = m.get(nombre);
 			
-			Persona personaMujerConMenorSalario = optionalDePersona.get();
+			m.put(nombre, frecuenciaDeOcurrencia == null ? 1 : frecuenciaDeOcurrencia++ );
 			
-			System.out.println("Persona Mujer con el menor salario " + 
-			              personaMujerConMenorSalario);
+			
 		}
-			
+		
 		
 	}
 }
