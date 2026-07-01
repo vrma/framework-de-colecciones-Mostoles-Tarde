db59752f833b7c0293baad06096473e44ccef836
vrma <vrmachado@gmail.com>
2026-07-01 18:02:59 +0200

    antes de crear la rama map_interface

--- framework-de-colecciones/src/main/java/com/example/App.java
+++ framework-de-colecciones/src/main/java/com/example/App.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -82,378 +83,78 @@ public class App {
 		personas.add(persona4);
 		personas.add(persona5);
 
-		/*
-		 * Obtener, utilizando Operaciones de Agregado, el salario promedio de todas las
-		 * personas del genero MUJER
-		 */
-
-		// Paso numero 1. Convertir la coleccion de personas, en este caso, en un flujo
-		// (stream)
-		// de elementos Persona, que va comenzar a circular por una tuberia (pipeline),
-		// es decir, una secuencia de operaciones, metodos de la clase Stream
-
-		/*
-		 * La coleccion se puede convertir en un flujo, utilizando el metodo stream() o
-		 * parallelStream(), este ultimo permitiria procesar los elementos de la
-		 * coleccion en un flujo paralelo, haciendo uso de todos los nucleos y los hilos
-		 * de ejecucion del procesador del equipo. No obstante, si la lista no es de
-		 * muchos elementos no se a notar gran diferencia entre utilizar el metodo
-		 * stream() o parallelStream()
-		 */
-
-		/*
-		 * Las Operaciones de Agregado, conjuntamente con las expresiones Lambda, los
-		 * metodos de la clase Stream, y los metodos por referencia es lo que se llama
-		 * tambien, la PROGRAMACION FUNCIONAL, que aunque Java no es un lenguaje
-		 * Funcional puro, la implementa conjutamente con la programacion orientada a
-		 * objetos.
-		 * 
-		 * En la PROGRAMACION FUNCIONAL, las funciones, los metodos se convierten en
-		 * "ciudadanos de primera clase", es decir, que al igual que con las variables,
-		 * se pueden pasar, las funciones, como parametro a los metodos y se pueden
-		 * devolver tambien. La PROGRAMACION FUNCIONAL, entre otras cosas, permite pasar
-		 * funcionalidad a los metodos, es un tipo de programacion declarativa, no
-		 * imperativa, si pide que es lo que se desea y el metodo responde a esa
-		 * peticion (request) de una forma concreta
-		 */
-
-		Stream<Persona> flujoDePersonas = personas.stream();
-
-		/* El metodo filter recibe lo que esta entre parentesis */
-
-		Predicate<? super Persona> predicate;
-
-		/*
-		 * Cuando en el diamante esta la palabra reservada super, a dicha coleccion se
-		 * le pueden agregar elementos, pero cuando la palabra reservada es extends a
-		 * dicha coleccion NO se le pueden agregar elementos, porque es una coleccion
-		 * inmutable Ejemplo a continuacion:
-		 */
-		List<? extends Persona> listaInmutable; // No se puede usar el metodo add() para
-												// agregar elementos
-		List<? super Persona> listaModificable; // Si se le pueden agregar elementos con
-												// el metodo add()
-
-		/*
-		 * Predicate es una interfaz fuPredicate<? ncional. ¿Que es una Interfaz
-		 * Funcional? Es una interface que tiene varios metodos (metodos con cuerpo,
-		 * implementados, no abstractos que pueden ser static, private, etc.), pero
-		 * solamente UN METODO ABSTRACTO
-		 * 
-		 * Concretamente la interfaz funcional Predicate tiene solamente un metodo
-		 * Abstracto, de todos los que tiene, dicho metodo se llama test(T t) y prueba
-		 * que el objeto que recibe como parametro cumpla una condicion determinada, y
-		 * si la cumple, el objeto pasara el siguiente nivel de la tuberia
-		 * 
-		 * A Continuacion explico lo que esta dentro del diamante <? super Persona>
-		 * 
-		 * Una lista que en el diamente tenga ? significa que es una coleccion que
-		 * admite cualquier tipo de elemento. El caracter ? se le llama comodin
-		 * 
-		 * List<?> listaDeCualquierCosa; Pero este lista es dificil de manejar
-		 * posteriormente, por lo que una coleccion que utilice el signo ? debe estar
-		 * acotada, por ejemplo
-		 * 
-		 * Collection<? super Persona> // Partimos de que tener una coleccion totalmente
-		 * generica no es una buena idea, sino que dicha coleccion tiene que estar
-		 * acotada, como seria este caso que indica que esta coleccion puede almacenar
-		 * elementos de cualquier tipo (?) pero que este acotado por encima (super) por
-		 * el tipo Persona, ES DECIR, que en este caso la coleccion admite elementos de
-		 * cualquier tipo donde la clase Persona sea el super tipo, es decir, la clase
-		 * base, la madre, y por supuest tipos Persona tambien va a admitir
-		 */
-
-		/*
-		 * El metodo filter esta gritando que le pasen como parametro "algo" que
-		 * implemente la interfaz Predicate, y de momento lo unico que implementa una
-		 * interfaz es una clase
-		 * 
-		 * Y vamos a comenzar implementando una clase externa llamada CriterioDeFiltro
-		 */
-
-		/*
-		 * IMPORTANTE!!! Crear una clase externa para satisfacer la condicion del metodo
-		 * filter, es una exageracion, porque el objetivo de dicha clase es utilizarla
-		 * en el metodo filter nada mas.
-		 * 
-		 * Entoces ¿Que se sugiere hacer? Se necesita un tipo de clase que se utilice en
-		 * el mismo lugar donde se necesita, no fuera de ahí, es decir, se necesita una
-		 * denominada CLASE ANONIMA, que es un tipo de clase que permite instanciar un
-		 * objetos en el mismo lugar que se implementa la clase
-		 * 
-		 * La clase anonima es como una expresion de clase, que se utiliza para
-		 * implementar una interfaz, aunque tambien se puede utilizar para implementar
-		 * una clase abstracta.
-		 * 
-		 * Antes de utilizar una clase anonima para implementar la interfaz Predicate
-		 * vamos a crear una clase anonima para implemtar una sencilla interfaz llamada
-		 * saludo
-		 * 
-		 * Una clase anonima es una expresion de clase, por lo cual tiene que terminar
-		 * en punto y coma
-		 */
-
-		Saludar saludar = new Saludar() {
-
-			@Override
-			public void misSaludosParaTi(String nombre) {
-				System.out.println("Hola " + nombre);
-
-			}
-
-		};
-
-		saludar.misSaludosParaTi("Maria");
-
-		/*
-		 * Ejercicio # 1 del martes 23 de junio.
-		 * 
-		 * Utilizando una clase anonima implementar la interfaz Predicate que necesita
-		 * el metodo filter, es decir, pasar el criterio de filtro al metodo filter
-		 * mediante una expresion de clase anonima
-		 */
-
-		/* Soluciones aportadas por Juan Carlos */
-
-		// Variante # 1
-
-		Predicate<Persona> predicate2 = new Predicate<Persona>() {
-
-			@Override
-			public boolean test(Persona persona) {
-				// TODO Auto-generated method stub
-				return persona.genero().equals(Genero.MUJER);
-			}
-		};
-
-		// Variante de solucion preferida
-//		flujoDePersonas.filter(new Predicate<Persona>() {
-//
-//			@Override
-//			public boolean test(Persona persona) {
-//				// TODO Auto-generated method stub
-//				return persona.genero().equals(Genero.MUJER);
-//			}
-//		});
-
-		/* Si la interface que se va a implementar con una clase anonima es una 
-		 * interfaz funcional, utilizar una clase anonima es todavia EXCESIVO,
-		 * demasiado codigo, pues 
-		 * ¿Que se utiliza entonces? Se utiliza una EXPRESION LAMBDA 
-		 * 
-		 * ¿Que es una expresion lambda?
-		 * 
-		 * Es como un metodo anonimo, se utiliza para pasar a un metodo la implementacion
-		 * del unico metodo abstracto que hay que implementar.
-		 * 
-		 * A continuacion la sintaxis de una expresion lambda */
+		/* Se puede ordenar una coleccion a medida que se recorre, y no dejarla permanentemente
+		 * ordenada como sucede con el metodo sort() de la clase Collections */
 		
-		 OptionalDouble optionalDelSalarioPromedio = flujoDePersonas
-				.filter(p -> p.genero().equals(Genero.MUJER))
-				.mapToDouble(persona -> persona.salario().doubleValue())
-				.average();
-		 
-		 /* ¿Que es un Optional? El tipo de dato Optional es una de las maravillas de las
-		  * versiones mas recientes de Java, surgue en la version 8 de Java.
-		  * El tipo Optional te proteje del temido NullPointerException, es decir, que tu
-		  * intentes trabajar con un objeto y este tome el valor NULL, y el problema es que
-		  * cuando en una operacion interviene un null todo se vuelve NULL.
-		  * 
-		  * Finalmente, el tipo Optional hay que verlo como una cajita de sorpresa, donde
-		  * puede venir el resultado esperado, que seria el salario promedio en este caso,
-		  * o seria un valor NULL porque el promedio no se pudo calcular, porque ninguna de 
-		  * las personas del genero MUJER tenian salario */
-		 
-		 if (optionalDelSalarioPromedio.isPresent()) {
-			 
-			 // De la cajita del Optional puedo extraer el salario promedio
-			 // sin ningun peligro
-			 
-			 double salarioPromedio = optionalDelSalarioPromedio.getAsDouble();
-			 
-			 System.out.println("El salario promedio es: " + salarioPromedio);
-		 }
+		// Ejemplo # 1. Ordenar la coleccion de personas mientras se recorre, segun el 
+		// el orden natural
 		
-		 /* Ejemplo # 1 del Miercoles 24 de Junio.
-		  * 
-		  * Utilizando Operaciones de Agregado, recorrer las lista de personas y 
-		  * obtener una nueva coleccion que contenga solamente los nombres de las
-		  * personas, pero sin duplicados */
-		 
-		 Set<String> nombresSinDuplicados = personas.stream()
-				 .map(p -> p.nombre())
-				 .collect(Collectors.toSet());
-		 
-		 /* Si al final de la tuberia se quiere obtener una nueva coleccion
-		  * , la operacion terminal tiene que ser el metodo collect(), que recibe la 
-		  * implementacion de la interfaz Collector a traves de una clase que tiene
-		  * el mismo nombre pero en plural, Collectors en este caso, que tendra a su vez
-		  * metodos estaticos, en la propia clase Collectors para trabajar con los elementos
-		  * que se colectan al final de la tuberia */
-		 
-		 /* Cuando la expresion lambda lo unico que hace es llamar al metodo que es 
-		  * quien realmente hace el trabajo, utilizar una expresion lambda es poco 
-		  * eficiente y redundante, por lo cual lo mejor es que el propio metodo haga 
-		  * el trabajo, sin ningun intermediario, es decir que lo correcto es pasar 
-		  * el metodo por referencia  */
-		 
-		 Set<String> nombresSinDuplicados2 = personas.stream()
-				 .map(Persona::nombre)
-				 .collect(Collectors.toSet());
-		 
-		 // A continuacion vamos a imprimir los elementos de la coleccion 
-		 // nombresSinDuplicados2
-		 
-		 System.out.println("Set de nombres sin duplicados: ");
-		 
-		 // nombresSinDuplicados2.stream().forEach(nombre -> System.out.println(nombre));
-		 
-		 /* En la sentencia anterior, la expresion lambda lo unico que hace es 
-		  * llamar al metodo println, por tanto se puede quitar la lambda y pasar el 
-		  * metodo por referencia 
-		  * 
-		  * En las ultimas versiones de Java, no hace falta el metodo stream() si directamente
-		  * se utiliza una operacion terminal a continuacion del origen de la tuberia
-		  * */
-		 
-		 nombresSinDuplicados2.forEach(System.out::println);
-		 
-		 /* OBJECT ORDERING (Ordenamiento de Objetos) 
-		  * 
-		  * https://docs.oracle.com/javase/tutorial/collections/interfaces/order.html 
-		  * 
-		  * Los algoritmos son una parte integral del framework de colecciones
-		  * 
-		  * A continuacion vamos a ver los algoritmos de ordenamiento (sort) implementados
-		  * en la clase Collections
-		  * 
-		  * Como ejemplo vamos a ordenar la lista de nombres siguiente segun el Orden 
-		  * Natural, lexicograficamente de la A a la Z
-		  * 
-		  * */
-		 
-		 List<String> nombres = Arrays.asList("Miguel", "Angel", 
-				 "Youssef", "Yodalis", "Elida", "Jakelin", "Juan", "Carlos", "Gina");
-		 
-		 System.out.println("Listado original de nombres: ");
-		 nombres.forEach(System.out::println);
-		 
-		 // Ordenamiento segun el orden natural (Natural Ordering)
-		 Collections.sort(nombres);
-				 
-		 System.out.println("Listado de nombres ordenado, alfabeticamente, lexicograficamente");
-		 nombres.forEach(System.out::println);
-		 
-		 /* Vamos a intentar ordenar la lista de personas 
-		  * ¿Que va a pasar? 
-		  * 
-		  * Que no es posible ordenar mi listado de personas, compuesto por elementos que son
-		  * record de Persona, ¿Por que entonces si se pudo ordenar las lista de nombres? 
-		  * Rta. Porque todos los tipos de datos de Java implementan la interfaz Comparable,
-		  * mientras que el tipo record Persona creado por nosotros no implementa la interfaz
-		  * Comparable, en resumen, que si no se implementa la interfaz Comparable el metodo
-		  * sort() no tiene medios para comparar los elementos del tipo de datos concreto.
-		  * 
-		  * IMPORTANTE!!! El Orden Natural (Natural Ordering) viene dado por la implementacion
-		  * de la interfaz Comparable, y si el tipo de datos no implementa dicha interfaz pues 
-		  * NO tiene orden natural, que se podrá ordenar de otra manera pero no segun 
-		  * el orden natural
-		  * 
-		  * ver la tabla que esta en el link suministrado inicialmente: */
-		 
-		 /* Se entiende que para ordenar la lista de personas, el tipo de datos de 
-		  * los elementos que conforman la lista de personas, que el record Persona 
-		  * tiene que implementar, si o si, la interfaz Comparable para que una coleccion
-		  * de tipo record de Persona pueda ser ordenada segun el Orden Natural */
+		System.out.println("Ejemplo # 1");
+		personas.stream()
+			.sorted()
+			.forEach(System.out::println);
 		
-		 System.out.println("Listado de personas sin ordenar");
-		 personas.forEach(System.out::println);
+		// Ejemplo # 2. Ordenar la coleccion de personas mientras se recorre por el salario
+		// de la persona
+		System.out.println("Ejemplo # 2");
+		personas.stream()
+			.sorted(Comparator.comparing(Persona::salario))
+			.forEach(System.out::println);
+
+		
+		/* Ejercicio # 1 del martes 30 de Junio.
+		 * 
+		 * Ordenar el listado de personas, por salario de mayor a menor salario */
+		
+		// Suministrado por Jakelin
+		
 		 
-		 Collections.sort(personas);
-		 
-		 System.out.println("Listado de personas ordenado segun el ORDEN NATURAL");
-		 personas.forEach(System.out::println); 
-		 
-		 /* Imaginate que el Jefe de nuestro Dpto NO necesita ordenar las personas
-		  * segun el ORDEN NATURAL del record Persona, sino que el necesita ordenar
-		  * la lista de personas por el salario, de mayor a menor, es decir, en orden
-		  * inverso. 
-		  * 
-		  * El problema es que en nuestro Dpto NO tenemos el codigo fuente del record 
-		  * Persona, para cambiar el ORDEN NATURAL. ¿Que podemos hacer entonces para 
-		  * ordenar las personas por el salario SIN modificar el ORDEN NATURAL?
-		  * * */
-		 
-		 /* Rta. Por suerte, el metodo sort() de la clase Collections puede recibir
-		  * un segundo parametro que seria el criterio de comparacion, para 
-		  * comparar dos personas sin que intervenga el ORDEN NATURAL */
-		 
-		 Collections.sort(personas, 
-				  (p1, p2) -> p1.salario().compareTo(p2.salario()));
-		 
-		 System.out.println("Listado de personas ordenado segun el comparator por el salario de"
-		 		+ "menor a mayor");
-		 personas.forEach(System.out::println);
-		 
-		 // ¿Como hacer para que muestre las personas de mayor salario primero?
-		 // Rta. En el cuerpo de la lambda cambiando el orden, primero la persona 2 
-		 // y luego la 1
-		 	
-		 Collections.sort(personas, 
-				  (p1, p2) -> p2.salario().compareTo(p1.salario()));
-		 
-		 System.out.println("Listado ordenado de mayor a menor salario");
-		 personas.forEach(System.out::println);
-		 
-		 /* Ejercicio # 1 del Lunes 29 de Junio.
-		  * 
-		  * Ordenar la coleccion de personas segun la edad de la persona de mayor a menor,
-		  * es decir, las personas mayores se muestren primero. */
-		 
-		 // Solucion aportada por Jakelin
-		 
-		 Collections.sort(personas, 
-					(p3, p4) -> p3.fechaNacimiento().compareTo(p4.fechaNacimiento()));
-			  System.out.println("Listado de personas ordenado segun la edad de la "
-			  		+ "persona de mayor a menor");
-			  personas.forEach( System.out::println);
-		 
-		 /* Ejercicio # 2 del Lunes 29 de Junio
-		  * 
-		  * Ordenar la coleccion de personas primero por el genero y luego por la edad, 
-		  * mostrando primero las personas mas jovenes de su genero. 
-		  * 
-		  * */
-			  
-		/* Para solucionar el ejercicio deberiamos crear un metodo que nos devuelva la edad
-		 * de la persona, en el record Persona */
-			  
-	    
-		 Collections.sort(personas, Comparator.comparing(Persona::genero,
-				        Comparator.nullsFirst(Comparator.naturalOrder()))
-				 .thenComparing(Persona::edad));
-		 
-		System.out.println("Solucion al ejercicio 2");
-		personas.forEach(System.out::println);
-		 
-		 /**
-		  * Ejercicio # 3 del Lunes 29 de Junio
-		  * 
-		  * Recorrer la coleccion de personas y obtener una nueva coleccion con las personas
-		  * del genero MUJER que nacieron en la segunda quincena del mes de su fecha de 
-		  * nacimiento.
-		  * 
-		  */
-		 
-		List<Persona> personasEjercicio3 = personas.stream()
-				.filter(p -> p.genero().equals(Genero.MUJER) && 
-				p.fechaNacimiento().with(TemporalAdjusters
-						.lastDayOfMonth()).minusDays(15).isBefore(p.fechaNacimiento()))
-		        .collect(Collectors.toList());
-		 
-		 System.out.println("Listado de personas del ejercicio # 3");
-		 personasEjercicio3.forEach(System.out::println);
-		 
+		/* Ordenar el listado de personas, por salario de mayor a menor salario */
+         System.out.println("ejercicio 1 ");
+         
+         personas.stream()
+	        .sorted(Comparator.comparing(Persona::salario).reversed())
+	        .forEach(System.out::println);
+		
+		/* Ejercicio # 2. Ordenar por Genero, primero y luego por salario, a medida que se
+		 * recorre la coleccion de persona para mostrarla */
+         
+         // Suministrado por Carlos
+        System.out.println("Ejercicio #2");
+ 		personas.stream()
+ 			.sorted(Comparator.comparing(Persona::genero)
+ 					.thenComparing(Persona::salario))
+ 			.forEach(System.out::println);
+		
+		
+		/* Ejercicio # 3. Mostrar la persona que tiene el mayor salario de todas las 
+		 * personas. */
+ 		System.out.println("Ejercicio 3. Persona con el mayor salario");
+		Optional<Persona> personaOptional = personas.stream()
+			.max(Comparator.comparing(Persona::salario));
+		
+		if (personaOptional.isPresent())
+		    System.out.println(personaOptional.get());
+		
+		/* Ejercicio # 4. 
+		 * 
+		 * Recuperar la persona del genero MUJER, que tenga el menor salario */
+		Optional<Persona>  optionalDePersona =  personas.stream()
+			.filter(persona -> persona.genero().equals(Genero.MUJER))
+			.min(Comparator.comparing(Persona::salario));
+		
+		if (optionalDePersona.isPresent()) {
+			
+			// Quiere decir que en la "cajita" del Optional esta presente  la persona
+			// que es del genero MUJER y que tiene el menor salario
+			
+			Persona personaMujerConMenorSalario = optionalDePersona.get();
+			
+			System.out.println("Persona Mujer con el menor salario " + 
+			              personaMujerConMenorSalario);
+		}
+			
+		
 	}
 }
 
